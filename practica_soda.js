07695d169e929848e7d7c991fd6d03fada39d990
Karol <karolcdcd@gmail.com>
2026-07-24 13:25:59 -0600

    docs: evidencia final y servidor actualizado

--- practica_soda.js
+++ practica_soda.js
@@ -1,3 +1,9 @@
 require('dotenv').config();
 const express = require('express');
+const app = express();
+
 const puerto = process.env.PORT || 3000;
+
+app.listen(puerto, () => {
+    console.log(`Servidor corriendo en el puerto ${puerto}`);
+});
